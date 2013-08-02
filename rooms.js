d3d2f975197ab6d00ca14abf731e5fedab95ad70
Cosym <mike-gaming@hotmail.co.uk>
2013-08-02 11:51:58 +0100

    Joim room crash check 3

--- rooms.js
+++ rooms.js
@@ -53,8 +53,7 @@ var GlobalRoom = (function() {
 
 		this.autojoin = []; // rooms that users autojoin upon connecting
 		this.staffAutojoin = []; // rooms that staff autojoin upon connecting
-		if (!room) console.log('crashing fucker: ' + i); continue;
-		for (var i=0; i<this.chatRoomData.length; i++) {
+		for (var i=0; i<this.chatRoomData.length; i++) { if (!room) console.log('crashing fucker: ' + i); continue;	
 			var id = toId(this.chatRoomData[i].title);
 			console.log("NEW CHATROOM: "+id);
 			var room = rooms[id] = new ChatRoom(id, this.chatRoomData[i].title, this.chatRoomData[i]);
