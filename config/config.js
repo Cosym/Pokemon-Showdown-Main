9fb39f655749aa04fd93a53d0e25b98e846d3aae
LilehBubbles <LilyBubbles7@gmail.com>
2013-09-22 14:48:24 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -159,6 +159,7 @@ exports.customavatars = {
 	'gymleadejk' : 'jk.png',
 	'gymleadeakahoshi' : 'aka.png',
 	'gymtraineumbro' : 'umbro.png',
+	'elitefoulilyr' : 'Lily.png'
 	'cosy' : 'cosy.png'
 	//'userid' : 'customavatar.png'
 };
