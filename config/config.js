e74410176095db3d9782989d16edaf6acca9a8d8
LilehBubbles <LilyBubbles7@gmail.com>
2013-09-23 17:24:25 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -161,6 +161,7 @@ exports.customavatars = {
 	'gymtraineumbro' : 'umbro.png',
 	'elitefoulilyr' : 'lily.png',
 	'elitefouvale' : 'vale.png',
+	'gymtraineruby' : 'ruby.png'
         'cosy' : 'cosy.png'
 	//'userid' : 'customavatar.png'
 };
